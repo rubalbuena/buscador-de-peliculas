e0eb4d28bc58ca50466b0e2f390a4d103b00bd23
rubalbuena <154548046+rubalbuena@users.noreply.github.com>
2024-03-14 14:38:50 -0300

    pelis.js
    en este archivo hice las funciones

---
+++ pelis.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+function getPeliculas() {
+    const jsonPeliculas = fs.readFileSync(__dirname +"/pelis.json");
+    const parsePeliculas = JSON.parse(jsonPeliculas);
+    return parsePeliculas;
+}
+exports.getSort = function(argumentoDeConsola) {
+    if (argumentoDeConsola.length === 0){
+        console.table(getPeliculas()) 
+        return ;
+    }
+    const pelisPorTitulo = getPeliculas().sort((a, b) => a.title.localeCompare(b.title));
+    const pelisPorTag = getPeliculas().sort((a, b) => a.tags - b.tags);
+    const peliPorRating = getPeliculas().sort((a, b) => a.rating - b.rating);
+    const resultado = argumentoDeConsola[1].toLowerCase();
+    if (argumentoDeConsola[0].toLowerCase().startsWith("--sort")){
+        if (resultado === "title"){
+        console.table(pelisPorTitulo);
+    }else if (resultado === "rating"){
+        console.table(peliPorRating);
+    }else if(resultado === "tag"){
+        console.table(pelisPorTag);
+    }
+    }
+    const pelisTag = getPeliculas().filter(pelicula =>{
+        return pelicula.tags.includes(argumentoDeConsola[1].toLowerCase());
+    });
+    if (argumentoDeConsola[0].toLowerCase().startsWith("--tag")){
+        console.table(pelisTag);
+    } 
+    const argumento = argumentoDeConsola[1];
+    const peliDeTitulo = getPeliculas().filter(pelicula =>{
+        return pelicula.title.includes(argumento);
+    });
+    if (argumentoDeConsola[0].startsWith("--search")){
+        console.table(peliDeTitulo)
+        
+    } 
+}   
